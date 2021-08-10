e72c879f51f6b65bbb840b6bc50ec4abd3401a60
ismaeelhaider72 <66254080+ismaeelhaider72@users.noreply.github.com>
2021-08-10 19:44:17 +0500

    Create vars.tf

---
+++ modules/vpc/vars.tf
@@ -0,0 +1,22 @@
+variable "vpc_cidr" {}
+
+variable "tenancy" {}
+
+variable "vpc_id" {}
+
+variable "subnet_cidr1" {}
+
+
+variable "subnet_cidr2" {}
+
+
+variable "subnet_cidr3" {}
+
+
+variable "subnet_cidr4" {}
+
+
+variable "subnet_cidr5" {}
+
+
+variable "subnet_cidr6" {}
